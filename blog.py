6f83d3cc91b5466d7ca400c610347621d99670e5
Pablojea <pablo_ojea@outlook.com>
2023-09-11 15:04:52 +0200

    creates a basic database manager

--- blog.py
+++ blog.py
@@ -1,1 +1,43 @@
-print('hello world')
+import traceback
+
+import mysql.connector
+import os
+
+
+class Dbm:
+
+    def __init__(self, host='localhost', user='root', database='postagram'):
+
+        self.host = host
+        self.user = user
+        self.password = os.environ['mypass']
+        self.database = database
+
+        try:
+            self.manager = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            self.cursor = self.manager.cursor()
+            print('DB manager instance created successfully')
+
+        except Exception as ex:
+            print('failed to connect to database')
+            print(traceback.print_exception())
+
+    def show_all_tables(self):
+        self.cursor.execute('SHOW TABLES')
+        for table in self.cursor:
+            print(table)
+
+    def get_all_posts(self):
+        self.cursor.execute('SELECT * FROM posts')
+        for post in self.cursor:
+            print(post)
+
+
+myDb = Dbm()
+myDb.show_all_tables()
+myDb.get_all_posts()
