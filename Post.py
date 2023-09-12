4df0290bcd12f0f5c3b5d540ea52819dcf863830
Pablojea <pablo_ojea@outlook.com>
2023-09-12 15:30:14 +0200

    adds simple Post class to manage data

---
+++ Post.py
@@ -0,0 +1,9 @@
+class Post:
+
+    def __init__(self, pid, title, brief, body, image, uid):
+        self.pid = pid
+        self.title = title
+        self.brief = brief
+        self.body = body
+        self.image = image
+        self.uid = uid
